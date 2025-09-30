aca73bd5556b57d7777ed2d3e6bbed3a5da842f4
John Alencar <john.wesley.alencar2@gmail.com>
2025-09-30 16:33:53 -0300

    Iniciando o projeto

---
+++ backend/user-service/user-service/src/main/java/com/projectflow/user_service/model/User.java
@@ -0,0 +1,32 @@
+package com.projectflow.user_service.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private String id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
---
+++ backend/user-service/user-service/src/main/resources/db/migration/V1__Create_Users_Tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
